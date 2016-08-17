004759131e348a3299b201383ed15751ac1d18bf
Leon Chea <leonchea2@gmail.com>
2016-08-17 17:18:00 +0930

    Add whitelist 101.166.220.202 to app

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Bletracking
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      config.web_console.whitelisted_ips = '101.166.220.202'
   end
 end
